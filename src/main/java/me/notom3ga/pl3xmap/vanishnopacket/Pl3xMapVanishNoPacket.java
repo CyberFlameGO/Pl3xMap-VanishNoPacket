b9b1d7f498b7feb531bcb38eeb0f70d855d87976
Ben Kerllenevich <me@notom3ga.me>
2021-02-15 17:44:50 -0500

    I might have been doing too much kotlin lately...

--- src/main/java/me/notom3ga/pl3xmap/vanishnopacket/Pl3xMapVanishNoPacket.java
+++ src/main/java/me/notom3ga/pl3xmap/vanishnopacket/Pl3xMapVanishNoPacket.java
@@ -7,11 +7,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.kitteh.vanish.VanishCheck;
-import org.kitteh.vanish.VanishManager;
 import org.kitteh.vanish.VanishPlugin;
 import org.kitteh.vanish.event.VanishStatusChangeEvent;
-import org.kitteh.vanish.staticaccess.VanishNoPacket;
 
 public class Pl3xMapVanishNoPacket extends JavaPlugin {
     @Override
@@ -39,7 +36,7 @@ public class Pl3xMapVanishNoPacket extends JavaPlugin {
             public void join(PlayerJoinEvent event) {
                 VanishPlugin vanish = (VanishPlugin) Bukkit.getPluginManager().getPlugin("VanishNoPacket");
                 Pl3xMapProvider.get().playerManager()
-                        .hidden(event.getPlayer().getUniqueId(), vanish.getManager().isVanished(event.getPlayer()))
+                        .hidden(event.getPlayer().getUniqueId(), vanish.getManager().isVanished(event.getPlayer()));
             }
         }, this);
     }
